3dfae04ba2ca334d1e53f9a4d4d0306940049917
Truebird0109 <159698889+Truebird0109@users.noreply.github.com>
2024-08-15 16:35:56 +0900

    Update send_arp.c
    들여쓰기 수정

--- send_arp.c
+++ send_arp.c
@@ -45,8 +45,8 @@ void usage() {
 
 /*찐 시작 */
 int main (int argc, char *argv[]) {
-	/*짝수 개수 일때만*/
-	if (argc < 4 || argc % 2 != 0)
+   /*짝수 개수 일때만*/
+   if (argc < 4 || argc % 2 != 0)
    {
       usage();
       return -1;
@@ -79,54 +79,52 @@ int main (int argc, char *argv[]) {
          }
       }
    }
-	/*패킷 설정*/
-	packet.eth.type = htons(0x0806); //0806 == arp
-    packet.arp.Hardware_type = htons(1); //하드웨어 타입 1 == 이더넷
-    packet.arp.Protocol_type = htons(0x0800); //프로토콜 타입 800 == ip 
-    packet.arp.Hw_addlen = 6;
-    packet.arp.Pro_addlen = 4;
-    packet.arp.Operation = htons(0x0001); //arp request임 
-    memset(packet.eth.dst, 0xff, 6); //ethernet dst mac ff채우기
-    memset(packet.arp.Dst_hwadd, 0x00, 6); //arp dst mac 00채우기
+   /*패킷 설정*/
+   packet.eth.type = htons(0x0806); //0806 == arp
+   packet.arp.Hardware_type = htons(1); //하드웨어 타입 1 == 이더넷
+   packet.arp.Protocol_type = htons(0x0800); //프로토콜 타입 800 == ip 
+   packet.arp.Hw_addlen = 6;
+   packet.arp.Pro_addlen = 4;
+   packet.arp.Operation = htons(0x0001); //arp request임 
+   memset(packet.eth.dst, 0xff, 6); //ethernet dst mac ff채우기
+   memset(packet.arp.Dst_hwadd, 0x00, 6); //arp dst mac 00채우기
 
-   	for (int i = 0 ; i < num_pair ; i++){
+   for (int i = 0 ; i < num_pair ; i++){
 
-		/* 가져온거 */
-		// MAC 주소를 가져오기 위해 필요한 소켓 설정
-		int fd = socket(AF_INET, SOCK_DGRAM, 0);
-		if (fd == -1) 
-		{
-			perror("socket");
-			return EXIT_FAILURE;
-		}
-		struct ifreq ifr;
-		memset(&ifr, 0, sizeof(ifr));
-		strncpy(ifr.ifr_name, dev, IFNAMSIZ - 1);
-		// ioctl을 통해 인터페이스의 MAC 주소 가져오기
-		if (ioctl(fd, SIOCGIFHWADDR, &ifr) == -1) {
-			perror("ioctl");
-			close(fd);
-			return EXIT_FAILURE;
-		}
-		close(fd);
-		// MAC 주소를 패킷 구조체에 복사
-		memcpy(packet.eth.src, ifr.ifr_hwaddr.sa_data, 6);
-		memcpy(packet.arp.Src_hwadd, ifr.ifr_hwaddr.sa_data, 6);
-		/* 가져온거 끝 */
+      /* 가져온거 */
+      // MAC 주소를 가져오기 위해 필요한 소켓 설정
+      int fd = socket(AF_INET, SOCK_DGRAM, 0);
+      if (fd == -1) 
+      {
+         perror("socket");
+         return EXIT_FAILURE;
+      }
+      struct ifreq ifr;
+      memset(&ifr, 0, sizeof(ifr));
+      strncpy(ifr.ifr_name, dev, IFNAMSIZ - 1);
+      // ioctl을 통해 인터페이스의 MAC 주소 가져오기
+      if (ioctl(fd, SIOCGIFHWADDR, &ifr) == -1) {
+         perror("ioctl");
+         close(fd);
+         return EXIT_FAILURE;
+      }
+      close(fd);
+      // MAC 주소를 패킷 구조체에 복사
+      memcpy(packet.eth.src, ifr.ifr_hwaddr.sa_data, 6);
+      memcpy(packet.arp.Src_hwadd, ifr.ifr_hwaddr.sa_data, 6);
+      /* 가져온거 끝 */
 
-		/* text 형식을 ip 형식으로 변신 */
-		inet_pton(AF_INET, sender[i], packet.arp.Src_proadd);
-		inet_pton(AF_INET, target[i], packet.arp.Dst_proadd);
-		
-		/* 패킷날리기 */
-        if (pcap_sendpacket(handle, (const u_char*)&packet, sizeof(packet)) != 0) {
-            fprintf(stderr, "Error sending the packet: %s\n", pcap_geterr(handle));
-            pcap_close(handle);
-            return EXIT_FAILURE;
-        }
-
-
-        printf("sender: %s, target: %s\n", sender[i], target[i]); 
+      /* text 형식을 ip 형식으로 변신 */
+      inet_pton(AF_INET, sender[i], packet.arp.Src_proadd);
+      inet_pton(AF_INET, target[i], packet.arp.Dst_proadd);
+      
+      /* 패킷날리기 */
+      if (pcap_sendpacket(handle, (const u_char*)&packet, sizeof(packet)) != 0) {
+         fprintf(stderr, "Error sending the packet: %s\n", pcap_geterr(handle));
+         pcap_close(handle);
+         return EXIT_FAILURE;
+      }
+      printf("sender: %s, target: %s\n", sender[i], target[i]); 
    }
-       pcap_close(handle);
+   pcap_close(handle);
 }
